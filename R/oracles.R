755647a646de82035da58155c4acfaabcd44da6d
Mathew Ling <m.ling@deakin.edu.au>
2019-09-30 22:01:52 +1000

    Extends the oracles

--- R/oracles.R
+++ R/oracles.R
@@ -1,18 +1,53 @@
 oracle_actions <- function() {
-  o_actions[sample(1:100, 1), ]
+  roll <-  sample(1:100, 1)
+  cat(o_actions[roll, 2], " (Roll: ", roll, ")", sep = "")
 }
 
 oracle_theme <- function() {
-  o_theme[sample(1:100, 1), ]
+  roll <-  sample(1:100, 1)
+  cat(o_theme[roll, 2], " (Roll: ", roll, ")", sep = "")
 }
 
 oracle_region <- function() {
-  o_region[sample(1:100, 1), ]
+  roll <-  sample(1:100, 1)
+  cat(o_region[roll, 2], " (Roll: ", roll, ")", sep = "")
 }
 
 oracle_location <- function() {
-  o_location[sample(1:100, 1), ]
+  roll <-  sample(1:100, 1)
+  cat(o_location[roll, 2], " (Roll: ", roll, ")", sep = "")
 }
+
+oracle_combat_actions <- function() {
+  roll <-  sample(1:100, 1)
+  cat(o_combat_actions[roll, 2], " (Roll: ", roll, ")", sep = "")
+}
+
+oracle_plot_twist <- function() {
+  
+  roll <-  sample(1:100, 1)
+  
+  if (roll<95) {
+  
+    cat(o_plot_twist[roll, 2], " (Roll: ", roll, ")", sep = "")  
+  }
+  
+  if (roll >= 95){
+    
+    dbl <- sample(1:100, 2, replace = TRUE)
+    
+    if(dbl[1] == dbl[2]){
+      cat("An extra dramatic twist: ", o_plot_twist[dbl[[1]], 2], " (Rolls: ", roll, ", ", dbl[[1]], ", ", dbl[[2]], ")", sep = "") 
+    }
+    
+    if(dbl[1] != dbl[2]) {
+      cat(o_plot_twist[dbl[[1]], 2], " AND ", o_plot_twist[dbl[[2]], 2], " (Rolls: ", roll, ", ", dbl[[1]], ", ", dbl[[2]], ")", sep = "") 
+    }
+    
+  }
+  
+}
+
 
 oracle_settlement_name <- function(quick = TRUE) {
   if (quick == TRUE){
